ae9f3b0524d695edf8867f3508127b5c2bd34872
bobilovv <bauer.manuel@outlook.com>
2017-05-16 08:08:58 +0200

    TDB laden und schreiben

--- src/main/java/model/TDBData.java
+++ src/main/java/model/TDBData.java
@@ -9,6 +9,7 @@ import org.apache.jena.atlas.lib.FileOps;
 import org.apache.jena.atlas.lib.StrUtils ;
 import org.apache.jena.graph.NodeFactory ;
 import org.apache.jena.query.* ;
+import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.RDFNode;
@@ -49,11 +50,35 @@ public class TDBData {
         FileManager fm = FileManager.get();
         fm.addLocatorClassLoader(TDBData.class.getClassLoader());
         InputStream in = fm.open("data/data.nt");
+        
+        // query for specific output Robert
+        String queryString = 
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/> " +
+                "SELECT ?name WHERE { " +
+                "    ?person foaf:mbox <mailto:O11din@example.org> . " +
+                "    ?person foaf:name ?name . " +
+                "}";
+            
 
         Location location = Location.create ("target/TDB");
         Dataset dataset = TDBFactory.createDataset(location);
         dataset.begin(ReadWrite.WRITE);
         try {
+        	
+        	//Einfügen in TDB 
+/*        	Model model = dataset.getDefaultModel();
+            model.createResource("http://example.org/Stefan", FOAF.Person)
+            .addProperty(FOAF.name, "Stefan")
+            .addProperty(FOAF.mbox, model.createResource("mailto:Mitmannsgruber@example.org"))
+            .addProperty(FOAF.knows, model.createResource("http://example.org/Stefan_M"));
+*/
+
+        	
+        	//Load inital Data
+/*        	
+        	TDBLoader.load(TDBInternal.getBaseDatasetGraphTDB(TDBFactory.createDatasetGraph(location)), in, false);
+*/
+        	
         	
             dataset.commit();
         } catch (Exception e) {
@@ -72,6 +97,26 @@ public class TDBData {
         } finally {
             dataset.end();
         }
+        
+        // take this try block for select via query string
+/*
+        try {
+            Query query = QueryFactory.create(queryString);
+            QueryExecution qexec = QueryExecutionFactory.create(query, dataset);
+            try {
+                ResultSet results = qexec.execSelect();
+                while ( results.hasNext() ) {
+                    QuerySolution soln = results.nextSolution();
+                    Literal name = soln.getLiteral("name");
+                    System.out.println(name);
+                }
+            } finally {
+                qexec.close();
+            }
+        } finally {
+            dataset.end();
+        }
+*/  
     }
 
 }
