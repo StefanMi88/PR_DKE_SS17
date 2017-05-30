d9f5e6e2a63944f0300e5f262b5e4e59837dfd2a
Odin <Odin@Odin>
2017-05-30 19:49:40 +0200

    .

--- src/main/java/model/Model.java
+++ src/main/java/model/Model.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Model {
+	
+	/**
+	 * 
+	 * @param Token zb "Robert"
+	 * @return Info infos über robert" Robert (Person) leer blablablablab
+	 */
 	public String getMeta(String Token){
 		//TODO: implement me
 		return Token;
 	}
+	
+	/**
+	 * 
+	 * @param Token zb "Robert"
+	 * @param Info infos über robert" Robert (Person) leer blablablablab
+	 * @return true if successful
+	 */
 	public boolean setmeta(String Token, String Info){
 		//TODO: implement me
 		return false;
