867c34e72363386a605b093beda06f63de7c29cc
StefanMi88 <stefan.mitmansgruber@gmail.com>
2017-06-10 15:59:59 +0200

    Updated Models

--- src/main/java/controller/SentenceDetection.java
+++ src/main/java/controller/SentenceDetection.java
@@ -11,6 +11,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 /**
  * Created by Stefan on 06.05.17.
  * Changed by Robert
@@ -20,57 +22,43 @@ public class SentenceDetection {
     public ArrayList<String> getTokenizedTxt(String txt){
     	ArrayList<String> tokentxt = new ArrayList<>();
     	try {
-            InputStream inputStream = new FileInputStream("Resources/en-sent.bin");
-            InputStream vocab = new FileInputStream("Resources/en-ner-person.bin");
-            TokenNameFinderModel vocabModel = new TokenNameFinderModel(vocab);
-            SentenceModel model = new SentenceModel(inputStream);
+    		//Input -> find Entities
             String sentence;
             if(txt.isEmpty()){
-            	sentence = "Hey! This is a testing sentence from Linz! I'm curious to see if it works. Just run the file... "
+            	sentence = "Hey! This is a testing sentence from Linz! I'm curious to see if it works. Just run the file... It is 10:00am! "
                         + "Robert, Manuel and Steve are working on this project!";
             }else{
             	sentence = txt;
             }
             
-
-            //Needed for splitting sentence
-            SentenceDetectorME detector = new SentenceDetectorME(model);
-
-            //Detect single sentences in sentence String
-            String[] splitString = detector.sentDetect(sentence);
-
-            //Detect beginning and ending of sentences in sentence
-            Span spans[] = detector.sentPosDetect(sentence);
-
             //Tokenize sentence
             SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
             String tokens[] = tokenizer.tokenize(sentence);
-
-            //Retrieve Entities in Sentence
-            NameFinderME vocabChecker = new NameFinderME(vocabModel);
-            Span vocabSpans[] = vocabChecker.find(tokens);
             
+            //Array of entities
+            Span vocabSpans[] = null;
             
-
-            //Combine splitString and spans to create output
-            String[] combined = new String[spans.length];
-            for (int i = 0; i < spans.length; i++) {
-                combined[i] = splitString[i] + " from: " + spans[i];
-            }
-            for (String string: combined) {
-                System.out.println(string);
-            }
+            //OpenNLP models
+            ArrayList<InputStream> vocabs = new ArrayList<InputStream>();
+            vocabs.add(new FileInputStream("Resources/en-ner-person.bin"));
+            vocabs.add(new FileInputStream("Resources/en-ner-time.bin"));
+            vocabs.add(new FileInputStream("Resources/en-ner-organization.bin"));
+            vocabs.add(new FileInputStream("Resources/en-ner-money.bin"));
+            vocabs.add(new FileInputStream("Resources/en-ner-location.bin"));
+            vocabs.add(new FileInputStream("Resources/en-ner-date.bin"));
             
+            for (InputStream iS: vocabs) {
+            	TokenNameFinderModel vocabModel = new TokenNameFinderModel(iS);             
+                vocabSpans = checkVocab(vocabSpans, tokens, vocabModel);
+            }        
             
             boolean done=false;
-            //System.out.println("\nFollowing tokens were found in the given sentence: ");
             for (String s: tokens) {
             	 for(Span i: vocabSpans) {
             		 if(s.equals(tokens[i.getStart()])){
             			 //System.out.println(i.toString());
             			 //System.out.println(s);
             			 tokentxt.add("<token>" + s);
-            			 
             			 done=true;
             		 }
             	 }
@@ -91,4 +79,15 @@ public class SentenceDetection {
 		
 		return tokentxt;
     }
+
+	private Span[] checkVocab(Span[] vocabSpans, String[] tokens, TokenNameFinderModel vocabModel) {
+		//Retrieve Entities in Sentence
+        NameFinderME vocabChecker = new NameFinderME(vocabModel);
+        Span found[] = vocabChecker.find(tokens);
+        for (Span i: found) {
+        	System.out.println(i.toString());
+        }
+        vocabSpans = ArrayUtils.addAll(vocabSpans, found); 
+        return vocabSpans;
+	}
 }
